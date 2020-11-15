abf07e3857f4b4c8745122ef35c61af45a175896
Charels Gregson <chasg42@gmail.com>
2020-11-15 16:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string){
+function logShout(string) {
   console.log(string.toUpperCase);
 }
 
-function logWhisper(string){
+function logWhisper(string) {
   console.log(string.toLowerCse);
 }
